7cf51e8ae237d078a7295b5aed8ccc80eeed539b
kirubhel <61353074+kirubhel@users.noreply.github.com>
2024-05-04 18:51:23 +0300

    no use effect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaTelegram, FaLinkedin ,FaGithub} from "react-icons/fa";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState,useEffect } from "react";
+import { useState } from "react";
 
 
 function Contact() {
